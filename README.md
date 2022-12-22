2e4db491438e75a5dece9852e0ffa4f975bad487
Dani <dan.avila7@gmail.com>
2022-12-21 21:48:38 -0500

    commentarios y readme.md

--- README.md
+++ README.md
@@ -13,6 +13,14 @@ $ npm install
 $ node index.js
 ```
 
+<hr>
+
 ## Tutorial
 
 https://youtu.be/XhkB5_ueb3M
+
+## Artículo con el paso a paso para ejecutar
+
+https://medium.com/@dan.avila7/bot-en-twitter-con-nodejs-ccc35a8914ca
+
+
--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const user_client = require('./client/user_client.js');
 const {ETwitterStreamEvent} = require('twitter-api-v2');
 
 async function main(){
-
+    
     //user
-    //const user = await user_client.v2.me();
+    const user = await user_client.v2.me();
     //console.log(user);
     let user_id = '1598408860720570384';
 
@@ -29,7 +29,13 @@ async function main(){
         console.log(tweet.data.author_id);
         console.log(tweet.data.text);
 
+        //like
         await user_client.v2.like(user_id, tweet.data.id);
+
+        //retweet
+        await user_client.v2.retweet(user_id, tweet.data.id);
+
+        //reply
         await user_client.v1.reply('I like this!', tweet.data.id);
         
     });
